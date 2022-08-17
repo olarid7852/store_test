6dc5aa741d409a45274970a39695a2f720087703
Ridwan Olawumi <olarid7852@gmail.com>
2022-08-17 15:57:08 +0000

    Implemented admin for order

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderAdmin(admin.ModelAdmin):
+    def has_change_permission(self, request, *args) -> bool:
+        if len(args) > 0:
+            return args[0].status != Order.OrderStatus.cancelled
+        return True
+
+admin.site.register(Order, OrderAdmin)
